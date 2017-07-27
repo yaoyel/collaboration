60830115ba5b3849b4348b9eeba99dea94188920
yebinch <bcye@chinahrd.net>
2017-07-27 10:27:22 +0800

    for deploy

--- server/controller/api/users.ts
+++
@@ -1,9 +0,0 @@
-import koaRouter=require('koa-router');
-const router=new koaRouter();
-
-import url=require('url');
-
-
-router.get('/',async(ctx,next)=>{
-    ctx.body= ctx.applicationServerKey
-})
